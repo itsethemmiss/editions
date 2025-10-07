a2f5404e44069df0f54c3fe31e6e2a94a72ac42b
Ethem Beldagli <198623801+fdsfasdf2314@users.noreply.github.com>
2025-10-07 09:47:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ethem = input("What type of ethem miss do you want: ").lower()
+ethem = input("What type of ethem miss do you want (miss, sir, beldagli): ").lower()
 if ethem == "miss":
     print("\nIts ethem miss")
 elif ethem == "sir":
